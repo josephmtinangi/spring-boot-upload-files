001780f9f7afa46af2093164b3d89d68720e7e24
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-03-29 10:54:43 +0300

    Initial commit

---
+++ src/main/java/io/github/josephmtinangi/springbootuploadfiles/SpringBootUploadFilesApplication.java
@@ -0,0 +1,12 @@
+package io.github.josephmtinangi.springbootuploadfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadFilesApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/josephmtinangi/springbootuploadfiles/SpringBootUploadFilesApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.josephmtinangi.springbootuploadfiles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootUploadFilesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
